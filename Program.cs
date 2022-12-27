a524426c81d40f27439373972d707ace727fe38d
Jackie Yi <mariomash1vg@gmail.com>
2022-12-27 15:39:29 -0500

    asdklfjsal

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Drawing.Common;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(GetColorAt(1,2));
+
+Color GetColorAt(int x, int y)
+{
+    Bitmap bmp = new Bitmap(1, 1);
+    Rectangle bounds = new Rectangle(x, y, 1, 1);
+    using (Graphics g = Graphics.FromImage(bmp))
+        g.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+    return bmp.GetPixel(0, 0);
+}
